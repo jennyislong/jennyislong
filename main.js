db7d5c3d550218e21003ebc7f469b37b1618d131
jennyislong <jenniilicious@gmail.com>
2021-01-02 15:14:54 -0500

    Mobile navbar collapse close when click link

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('.header').height($(window).height());
 })
 
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
+});
 
 $(".navbar a").click(function(){
     $("body,html").animate({
